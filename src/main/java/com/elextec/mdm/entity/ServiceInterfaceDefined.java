d98e11c1a45c088e2319818641246bdb61c42489
zhangkaijun <116762952@qq.com>
2018-01-18 09:47:23 +0800

    WS，存储过程接口定义表结构修改

---
+++ src/main/java/com/elextec/mdm/entity/ServiceInterfaceDefined.java
@@ -0,0 +1,98 @@
+package com.elextec.mdm.entity;
+
+import com.elextec.mdm.common.entity.BasicEntity;
+
+/**
+ * @author zhangkj
+ *
+ */
+public class ServiceInterfaceDefined extends BasicEntity{
+
+	private String type;//接口类型,0 webservice,1 存储过程
+	private String wsdlLocation;//WSDL地址
+	private String dburl;//如果接口是存储过程，需要填入数据库连接字符串
+	private String username;
+	private String password;
+	private String modelId;
+	private String bsId;
+	private String operationType;//操作类型，0拉取，1提送
+	private String operation;//webservice的接口方法或者存储过程方法
+	private String wsbinding;//webservice binding
+	private String bingNamespace;//webservice binding namespance
+	private String operationNamespace;//webservice operation namespance
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getWsdlLocation() {
+		return wsdlLocation;
+	}
+	public void setWsdlLocation(String wsdlLocation) {
+		this.wsdlLocation = wsdlLocation;
+	}
+	public String getDburl() {
+		return dburl;
+	}
+	public void setDburl(String dburl) {
+		this.dburl = dburl;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getModelId() {
+		return modelId;
+	}
+	public void setModelId(String modelId) {
+		this.modelId = modelId;
+	}
+	public String getBsId() {
+		return bsId;
+	}
+	public void setBsId(String bsId) {
+		this.bsId = bsId;
+	}
+	public String getOperationType() {
+		return operationType;
+	}
+	public void setOperationType(String operationType) {
+		this.operationType = operationType;
+	}
+	public String getOperation() {
+		return operation;
+	}
+	public void setOperation(String operation) {
+		this.operation = operation;
+	}
+	public String getWsbinding() {
+		return wsbinding;
+	}
+	public void setWsbinding(String wsbinding) {
+		this.wsbinding = wsbinding;
+	}
+	public String getBingNamespace() {
+		return bingNamespace;
+	}
+	public void setBingNamespace(String bingNamespace) {
+		this.bingNamespace = bingNamespace;
+	}
+	public String getOperationNamespace() {
+		return operationNamespace;
+	}
+	public void setOperationNamespace(String operationNamespace) {
+		this.operationNamespace = operationNamespace;
+	}
+	
+	
+}
---
+++ src/main/java/com/elextec/mdm/entity/ServiceInterfaceParam.java
@@ -0,0 +1,41 @@
+package com.elextec.mdm.entity;
+
+import com.elextec.mdm.common.entity.BasicEntity;
+
+/**
+ * @author zhangkj
+ *
+ */
+public class ServiceInterfaceParam extends BasicEntity{
+	
+	private String serviceDefinedId;
+	private String ioType;//输入输出类别，0输入，1输出
+	private String dataType;//参数数据类型，int,string等基本型以及complex复杂型
+	private String tableName;
+	
+	public String getServiceDefinedId() {
+		return serviceDefinedId;
+	}
+	public void setServiceDefinedId(String serviceDefinedId) {
+		this.serviceDefinedId = serviceDefinedId;
+	}
+	public String getIoType() {
+		return ioType;
+	}
+	public void setIoType(String ioType) {
+		this.ioType = ioType;
+	}
+	public String getDataType() {
+		return dataType;
+	}
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
+	}
+	public String getTableName() {
+		return tableName;
+	}
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+	
+}
