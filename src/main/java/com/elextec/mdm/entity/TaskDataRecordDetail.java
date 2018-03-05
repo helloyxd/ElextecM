48610f8e41102abc5ce9b34657622a9a8015272a
zhangkaijun <116762952@qq.com>
2018-03-05 15:39:17 +0800

    新增实体流程数据模块

---
+++ src/main/java/com/elextec/mdm/entity/TaskDataRecordDetail.java
@@ -0,0 +1,59 @@
+package com.elextec.mdm.entity;
+
+import java.util.Date;
+
+import com.elextec.mdm.common.entity.BasicEntity;
+
+public class TaskDataRecordDetail extends BasicEntity {
+
+	private String flowId;
+	private String dataId;
+	private String modelId;
+	private String systemId;
+	private String taskType;
+	private String remark;
+	private Date endTime;
+	
+	public String getFlowId() {
+		return flowId;
+	}
+	public void setFlowId(String flowId) {
+		this.flowId = flowId;
+	}
+	public String getDataId() {
+		return dataId;
+	}
+	public void setDataId(String dataId) {
+		this.dataId = dataId;
+	}
+	public String getModelId() {
+		return modelId;
+	}
+	public void setModelId(String modelId) {
+		this.modelId = modelId;
+	}
+	public String getSystemId() {
+		return systemId;
+	}
+	public void setSystemId(String systemId) {
+		this.systemId = systemId;
+	}
+	public String getTaskType() {
+		return taskType;
+	}
+	public void setTaskType(String taskType) {
+		this.taskType = taskType;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public Date getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+}
---
+++ src/main/java/com/elextec/mdm/entity/TaskDataRecordSummary.java
@@ -0,0 +1,52 @@
+package com.elextec.mdm.entity;
+
+import com.elextec.mdm.common.entity.BasicEntity;
+
+public class TaskDataRecordSummary extends BasicEntity {
+
+	private String flowId;
+	private String modelId;
+	private String taskType;
+	private int successNum;
+	private int failNum;
+	private String remark;
+	
+	public String getFlowId() {
+		return flowId;
+	}
+	public void setFlowId(String flowId) {
+		this.flowId = flowId;
+	}
+	public String getModelId() {
+		return modelId;
+	}
+	public void setModelId(String modelId) {
+		this.modelId = modelId;
+	}
+	public String getTaskType() {
+		return taskType;
+	}
+	public void setTaskType(String taskType) {
+		this.taskType = taskType;
+	}
+	public int getSuccessNum() {
+		return successNum;
+	}
+	public void setSuccessNum(int successNum) {
+		this.successNum = successNum;
+	}
+	public int getFailNum() {
+		return failNum;
+	}
+	public void setFailNum(int failNum) {
+		this.failNum = failNum;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	
+	
+}
