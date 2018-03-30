abd4e97748b53ba6d95718d2a4e0a74ebd519123
helloyxd <helloxd@qq.com>
2018-03-30 13:38:59 +0800

    更新

---
+++ src/main/java/org/springframework/boot/autoconfigure/security/SecurityAutoConfiguration.java
@@ -0,0 +1,6 @@
+package org.springframework.boot.autoconfigure.security;
+
+public class SecurityAutoConfiguration extends org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration{
+
+	
+}
